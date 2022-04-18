ad3b0af6640b0c4799b692a78c4a999033f02674
GreenDevth <theerapong@rumail.ru.ac.th>
2022-04-18 15:04:43 +0700

    add new status

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -89,36 +89,36 @@ class ScumServerStatus(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.change_presence.start()
-
-    @tasks.loop(seconds=5)
-    async def change_presence(self):
-        def get_players():
-            try:
-                response = requests.get('https://api.battlemetrics.com/servers/13458708')
-                status = response.status_code
-                if status == 200:
-                    print(response.json()['data']['attributes']['players'])
-                    player = response.json()['data']['attributes']['players']
-                    msg = f"{player}/20 Prisoner"
-                    return msg.strip()
-                elif status != 200:
-                    msg = "stay offline"
-                    return msg.strip()
-                else:
-                    msg = "wait for online"
-                    return msg.strip()
-            except Exception as e:
-                print(e)
-                return 0
-
-        result = get_players()
-        await self.bot.change_presence(
-            status=discord.Status.online,
-            activity=discord.Activity(type=discord.ActivityType.watching, name=result)
-        )
+    # @commands.Cog.listener()
+    # async def on_ready(self):
+    #     self.change_presence.start()
+    #
+    # @tasks.loop(seconds=15)
+    # async def change_presence(self):
+    #     def get_players():
+    #         try:
+    #             response = requests.get('https://api.battlemetrics.com/servers/13458708')
+    #             status = response.status_code
+    #             if status == 200:
+    #                 print(response.json()['data']['attributes']['players'])
+    #                 player = response.json()['data']['attributes']['players']
+    #                 msg = f"{player}/20 Prisoner"
+    #                 return msg.strip()
+    #             elif status != 200:
+    #                 msg = "stay offline"
+    #                 return msg.strip()
+    #             else:
+    #                 msg = "wait for online"
+    #                 return msg.strip()
+    #         except Exception as e:
+    #             print(e)
+    #             return 0
+    # 
+    #     result = get_players()
+    #     await self.bot.change_presence(
+    #         status=discord.Status.online,
+    #         activity=discord.Activity(type=discord.ActivityType.watching, name=result)
+    #     )
 
     @commands.command(name='server')
     @commands.has_role("Verify Members")
